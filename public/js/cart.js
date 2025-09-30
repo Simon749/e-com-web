95ca8edbecda6d2c8b7b202902ef3348c6949036
Simon Mwangi <simonmwangi749@gmail.com>
2025-09-30 17:38:34 +0300

    commented out stripe

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,22 +1,11 @@
 const payBtn = document.querySelector('.checkout-btn');
 
 payBtn.addEventListener('click', () => {
-    fetch('/stripe-checkout', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            items: JSON.parse(localStorage.getItem('cartItems'))
-        }),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        if (data.url) {
-            window.location.href = data.url;
-        } else {
-            console.error('Invalid URL received from server:', data.url);
-        }
-    })
-    .catch((err) => console.error(err));
+    const payBtn = document.querySelector('.checkout-btn');
+
+payBtn.addEventListener('click', () => {
+    // Simulate a redirect for demo purposes
+    window.location.href = '../success.html'; 
+});
+
 });
