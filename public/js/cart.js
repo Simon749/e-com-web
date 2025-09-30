544ed411f4f84f1244ebbbb0a3bd7f8f7c08e833
Simon Mwangi <simonmwangi749@gmail.com>
2025-09-30 18:09:40 +0300

    ...

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,9 +3,10 @@ const payBtn = document.querySelector('.checkout-btn');
 payBtn.addEventListener('click', () => {
     const payBtn = document.querySelector('.checkout-btn');
 
-payBtn.addEventListener('click', () => {
-    // Simulate a redirect for demo purposes
-    window.location.href = '../success.html'; 
-});
+    payBtn.addEventListener('click', () => {
+        // Simulate checkout success for demo purposes
+        window.location.href = '/success.html';
+    });
+
 
 });
